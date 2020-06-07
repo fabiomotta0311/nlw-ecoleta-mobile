8fc129eb0c60bf717e80ba41f160ed98c7d39535
Fabio Motta <fabiomotta0311@gmail.com>
2020-06-07 18:08:22 -0300

    upd

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';7
+import React from 'react';
 import { Feather as Icon } from '@expo/vector-icons'
 import { View, ImageBackground ,Image, StyleSheet, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
